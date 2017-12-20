29a04cbe46e6975c3fbf74c081eec607507f0eae
Sean Oughton <seanoughton@gmail.com>
2017-12-20 17:49:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const app = "I don't do much."
  }
  
  function removeLastKitten() {
-   var newArray = kittens.slice(0,-1)
+   var newArray = kittens.slice(0,(kittens.length-1) )
    return newArray
  }
  
