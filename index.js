775b5c89a3103f1c7fd93d8e6bc0c5ea19529ab4
Sean Oughton <seanoughton@gmail.com>
2017-12-20 17:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = "I don't do much."
 
  
  function kittens(){
-  kittens =   ['Milo', 'Otis', 'Garfield']
+   var kittens =   ['Milo', 'Otis', 'Garfield']
    return kittens
  }
  
   kittens()
  
  function destructivelyAppendKitten(name){
+   kittens.push(name)
   
  }
